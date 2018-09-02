d5b579561270b83ebd7dfce05870e6a8ac89bada
tomazz <ztomaz108@gmail.com>
2018-09-02 21:12:05 +0200

    a1

---
+++ test.sh
@@ -0,0 +1,28 @@
+git init
+touch a1.txt
+git add *
+git commit -m'a1'
+touch a2.txt
+git add *
+git commit -m'a2'
+touch a3.txt
+git add *
+git commit -m'a3'
+touch a4.txt
+git add *
+git commit -m'a4'
+touch a5.txt
+git add *
+git commit -m'a5'
+touch a6.txt
+git add *
+git commit -m'a6'
+touch a7.txt
+git add *
+git commit -m'a7'
+touch a8.txt
+git add *
+git commit -m'a8'
+touch a9.txt
+git add *
+git commit -m'a9'
